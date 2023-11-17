5a74b51b45394a2f4f0d8cd036572ea06b70a648
Ekhlas Uddin Ahmed <matrixedu14@gmail.com>
2023-11-18 00:18:45 +0600

    basic project setup done

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response, Application } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+
+  console.log(a);
+
+  res.send(a);
+});
+
+export default app;
